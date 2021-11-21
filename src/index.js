86bdd85150753105676d39f879378c270151062e
Kwessi Annor <kayannor@gmail.com>
2021-11-21 15:21:31 -0500

    Chore: update GraphQL server uri for production

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Pages from './pages'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  // uri: 'https://apollo-castronauts.herokuapp.com/',
+  // uri: 'http://localhost:4000/',
+  uri: 'https://apollo-castronauts.herokuapp.com/',
   cache: new InMemoryCache(),
 })
 
